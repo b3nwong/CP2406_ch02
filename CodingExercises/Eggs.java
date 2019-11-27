e53909f303a0999a706963506473276d3e218196
b3nwong <53283480+b3nwong@users.noreply.github.com>
2019-11-27 14:37:23 +0800

    finish question 8 eggs class

--- CodingExercises/Eggs.java
+++ CodingExercises/Eggs.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Eggs {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("How many eggs would you like to purchase?: ");
+        int eggs = Integer.parseInt(reader.nextLine());
+        //asks user to input number of eggs they would like to order
+
+        int dozenEggs = eggs/12;
+        //sorts eggs by the dozen
+
+        int looseEggs = eggs%12;
+        //accounts for the loose eggs not in the dozen
+
+        float finalPrice = (float) (dozenEggs*3.25 + looseEggs*.45);
+        //calculate price paid for the dozens and loose eggs
+
+        System.out.println("You ordered " + eggs + " eggs. That's " + dozenEggs + " dozen at $3.25 per dozen and " + looseEggs + " loose eggs at $0.45 each for a total of $" + finalPrice + ".");
+
+    }
 }
